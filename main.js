9c150b93e1009c5047d7b7672bf1f2eae00a386f
Dawitlior <dawitlior777@gmail.com>
2022-06-01 21:39:23 +0300

    add

--- main.js
+++ main.js
@@ -1,41 +1,91 @@
-function myFirstEvent(){
-    var myh1 = document.getElementById("my_h1");
-    myh1 = alert("hello world");
+// function myFirstEvent(){
+//     var myh1 = document.getElementById("my_h1");
+//     myh1 = alert("hello world");
+// }
+
+// function mySecondEvent(){
+// var myP = document.getElementById("my_p");
+// myP = console.log("lior dawit");
+// }
+
+// function myThirdEvent(){
+// var mySpan = document.getElementById("my_span")
+// mySpan.style.color = "red"
+// }
+
+// function myFourEvent(){
+//     var myH3 = document.getElementById("my_h1-2");
+//     myH3.style.width = "20vw";
+// }
+
+// function myFifthEvent(){
+//     var tar5 = document.getElementById("id_h5");
+//     tar5 = document.body.innerHTML = "champions"
+// }
+
+// function mySixEvent(){
+//     var userName = prompt("type sone name");
+//     document.write(userName)
+// }
+
+// function mySevenEvent(){
+//     var myTime = new Date();
+//     var theTimeNow = myTime.getHours();
+//     if(theTimeNow < myTime){
+//         document.body.style.backgroundColor = "yellow";
+//         document.body.innerHTML = "<h1>"+"good day"+"<h1/>"
+//     }
+//     else{
+//         document.body.style.backgroundColor = "blue";
+//         document.body.innerHTML = "<h1>" + "good evening" + "<h1/>";
+//     }
+// }
+// mySevenEvent();
+
+
+
+// function someFunctionOnEvent(){
+// var clickMe = document.getElementById("my_p");
+// clickMe.style = "cursor: pointer"
+
+// clickMe.onclick = function(){
+//     var userAge = prompt("what your age");
+// if(userAge > 18){
+//   clickMe.innerHTML = "<h1>" + "welcome" + "</h1>" 
+//   clickMe.style.color = "red";
+// }
+// else{
+//  clickMe.innerHTML = "<h3>" + "welcome" + "</h3>" 
+//   clickMe.style.color = "blue";
+// }
+// }
+// }
+// function mouseOver1(){
+//     var a1 = document.getElementById("my_p");
+//     a1.style.color = "green";
+// }
+
+
+// 9.	צרו תוכנית לוטו, התוכנית מקבלת מהמשתמש מספר בין 0 ל - 56 ובעת לחיצה על כפתור מדפיסה אותו למסך, הפעולה קורית 5 פעמים. לאחר מכן, התוכנית מגרילה מספר בין 0 ל 56 ומדפיסה אותו למסך.
+
+function getNumberAndPrint(){
+var first = document.getElementById("myBtn");
+for(var i = 0 ; i<5 ; i++){
+    first = prompt("type Number betwee 0-56");
+    document.body.innerHTML +="<br>" + first;
+}
+}
+function clickToLottyNumber(){
+    var second = getElementById("myBtn2");
+    second = Math.floor(Math.random()*56);
+    document.body.innerText = second;
 }
 
-function mySecondEvent(){
-var myP = document.getElementById("my_p");
-myP = console.log("lior dawit");
-}
 
-function myThirdEvent(){
-var mySpan = document.getElementById("my_span")
-mySpan.style.color = "red"
-}
 
-function myFourEvent(){
-    var myH3 = document.getElementById("my_h1-2");
-    myH3.style.width = "20vw";
-}
 
-function myFifthEvent(){
-    var tar5 = document.getElementById("id_h5");
-    tar5 = document.body.innerHTML = "champions"
-}
 
-function mySixEvent(){
-    var userName = prompt("type sone name");
-    document.write(userName)
-}
 
-function mySevenEvent(){
-    var myTime = new Date()
-    if(myTime <12){
-        document.body.style.backgroundColor = "yellow";
-        document.body.innerHTML = "<h1>"+"good day"+"<h1/>"
-    }
-    else{
-        document.body.style.backgroundColor = "blue";
-   
-    }
-}
+
+
+
